609b49a87c428417d32f473f6ece153075ab34a3
John Swinbank <john@swinbank.org>
2016-07-24 21:48:07 -0400

    Add a default date format.

--- jdsnap/date.py
+++ jdsnap/date.py
@@ -1,17 +1,19 @@
 import datetime
 
-__all__ = ["DAY", "WEEK", "MONTH", "YEAR", "date_from_name"]
+__all__ = ["DAY", "WEEK", "MONTH", "YEAR", "DEFAULT_DATEFMT", "date_from_name"]
 
 DAY = datetime.timedelta(days=1)
 WEEK = datetime.timedelta(weeks=1)
 MONTH = datetime.timedelta(days=30)
 YEAR = datetime.timedelta(days=365.25)
 
-def date_from_name(archive_name, prefix, fmtstring="%Y-%m-%d_%H-%M-%S"):
+DEFAULT_DATEFMT = "%Y-%m-%d_%H-%M-%S"
+
+def date_from_name(archive_name, prefix, datefmt=DEFAULT_DATEFMT):
     """
     Parse date from archive name.
 
     Given an archive named (prefix)-(date), extract and return the date as a
     (timezone unencumbered) datetime.
     """
-    return datetime.datetime.strptime(archive_name[len(prefix):], fmtstring)
+    return datetime.datetime.strptime(archive_name[len(prefix):], datefmt)
--- jdsnap/tests/test_date.py
+++ jdsnap/tests/test_date.py
@@ -1,30 +1,29 @@
 import datetime
 import unittest
-import jdsnap
+
+from jdsnap import (date_from_name, DEFAULT_DATEFMT,
+                    DAY, WEEK, MONTH, YEAR)
 
 class TestParseDate(unittest.TestCase):
     def setUp(self):
         self.date = datetime.datetime(2014, 1, 1, 0, 0, 0)
 
     def test_parse_simple(self):
-        date = jdsnap.date_from_name("prefix-%s" %
-                                     (self.date.strftime("%Y-%m-%d_%H-%M-%S")),
-                                     "prefix-")
+        date = date_from_name("prefix-%s" %
+                              (self.date.strftime(DEFAULT_DATEFMT)), "prefix-")
         self.assertEqual(self.date, date)
 
     def test_parse_no_prefix(self):
-        date = jdsnap.date_from_name("%s" %
-                                     (self.date.strftime("%Y-%m-%d_%H-%M-%S")),
-                                     "")
+        date = date_from_name("%s" % (self.date.strftime(DEFAULT_DATEFMT)), "")
         self.assertEqual(self.date, date)
 
     def test_parse_custom_format(self):
-        date = jdsnap.date_from_name("prefix-%s" %
-                                     (self.date.strftime("%d-%m-%Y_%S-%M-%H")),
-                                     "prefix-", fmtstring="%d-%m-%Y_%S-%M-%H")
+        date = date_from_name("prefix-%s" %
+                              (self.date.strftime("%d-%m-%Y_%S-%M-%H")),
+                              "prefix-", datefmt="%d-%m-%Y_%S-%M-%H")
         self.assertEqual(self.date, date)
 
 
 class TestConstants(unittest.TestCase):
     def test_relative_sizes(self):
-        self.assertTrue(jdsnap.DAY < jdsnap.WEEK < jdsnap.MONTH < jdsnap.YEAR)
+        self.assertTrue(DAY < WEEK < MONTH < YEAR)
